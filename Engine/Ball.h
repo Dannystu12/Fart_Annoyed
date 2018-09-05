15b7c7ce8c4d917581217f20057f6f5f9f7ba77b
Dannystu12 <mgdog1@googlemail.com>
2018-09-05 20:39:30 +0100

    Created Ball class

---
+++ Engine/Ball.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Vec2.h"
+#include "Graphics.h"
+class Ball
+{
+public:
+	Ball(const Vec2& pos, const Vec2& vel);
+	void Draw(Graphics& gfx) const;
+private:
+	Vec2 pos;
+	Vec2 vel;
+	static constexpr float radius = 7.0f;
+};
